00bd8086e0000dd9a952ae155400dc9b1a5f980c
paulsiskind <paulsiskind@gmail.com>
2015-11-12 19:28:26 -0700

    needs parsing function

--- routes/index.js
+++ routes/index.js
@@ -37,4 +37,38 @@ router.get('/api/v1/memories', function(req, res, next) {
   });
 });
 
+router.get('/api/v1/memories/:year', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    client.query('SELECT * FROM memories WHERE year = $1', [req.params.year], function(err, result) {
+      done();
+      res.status(200).json(helper.formatResponse(result));
+      if (err) {
+        return console.error('error running query', err);
+      }
+      console.log('connected to db')
+    });
+  });
+});
+
+router.get('/api/v1/memories/years', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    client.query('select year from memories;', function(err, result) {
+      done();
+      res.json(result.rows)
+      if (err) {
+        return console.error('error running query', err);
+      }
+      console.log('connected to db')
+    });
+  });
+});
+
+
+
 module.exports = router;
