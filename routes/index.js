cd62807f9ac077539d1de1c6077e98dde28f5dc2
paulsiskind <paulsiskind@gmail.com>
2015-11-12 17:26:00 -0700

    finished get route

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,40 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
+var conString = process.env.DATABASE_URL;
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+/* GET users listing. */
+router.post('/api/v1/memories', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    client.query('INSERT into memories(old_days, these_days, year) VALUES($1,$2,$3) RETURNING id;', [req.body.data.attributes.old_days, req.body.data.attributes.these_days, req.body.data.attributes.year], function(err, result) {
+      done();
+      res.json(result.rows)
+      if (err) {
+        return console.error('error running query', err);
+      }
+      console.log('connected to db')
+    });
+  });
+});
+/* GET users listing. */
+router.get('/api/v1/memories', function(req, res, next) {
+  pg.connect(conString, function(err, client, done) {
+    if (err) {
+      return console.error('error fetching client from pool', err);
+    }
+    client.query('select * from memories;', function(err, result) {
+      done();
+      res.json(result.rows)
+      if (err) {
+        return console.error('error running query', err);
+      }
+      console.log('connected to db')
+    });
+  });
 });
 
 module.exports = router;
